004e8ed2adac1946aeb649ce2f8ede253ee2489c
rennan-sbarros <rennan_sbarros@hotmail.com>
2021-09-24 17:31:53 -0300

    Criação dos componentes, estilização inicial e chamadas HTTP

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App() {
-    return <h1>Olá</h1>
+    return (
+        <RepositoryList />
+    )
 }
